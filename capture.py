a750e15d7920ddad58d92162db85d81d05a24015
Barak Obama <barak.oh16@gmail.com>
2017-05-19 02:37:34 +0300

    initial commit

--- capture.py
+++ capture.py
@@ -8,13 +8,18 @@ _, img = cam.read()
 g = image_processing.GripPipeline()
 
 
+
 g.process(img)
+
+contours_img = img.copy()
+center_img = img.copy()
+
 contours = g.find_contours_output
 cv2.imwrite('image.png', img)
-cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-cv2.imwrite('image_contours.png', img)
+cv2.drawContours(contours_img, contours, -1, (0, 255, 0), 3)
+cv2.imwrite('image_contours.png', contours_img)
 center = find_position(contours)
 center = (int(center[0] * WIDTH), int(center[1] * HEIGHT))
-cv2.circle(img, center, 2, (0, 255, 0), thickness=1, lineType=8, shift=0)
-cv2.imwrite("image_center.png", img)
+cv2.circle(center_img, center, 2, (0, 255, 0), thickness=1, lineType=8, shift=0)
+cv2.imwrite("image_center.png", center_img)
 
