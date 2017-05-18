9be871e51b4c87cd970a2ad7e7d9d66a52f6b3ba
Barak Obama <barak.oh16@gmail.com>
2017-05-19 00:26:03 +0300

    initial commit

---
+++ motor_drive/motor_drive.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo rightServo, leftServo;
+
+int rightPort = 11, leftPort = 10;
+int rightSignal = 90, leftSignal = 90;
+String income, left, right;
+
+void setup() {
+  Serial.begin(9600);
+  rightServo.attach(rightPort);
+  leftServo.attach(leftPort);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    income = Serial.readString();
+    right = income.substring(0, 3);
+    left = income.substring(3, 6);
+    rightSignal = right.toInt();
+    leftSignal = left.toInt();
+  }
+  rightServo.write(rightSignal);
+  leftServo.write(leftSignal);
+}
