f202e4a35713c3c47bf4d8de92a01745bdb89434
Barak Obama <barak.oh16@gmail.com>
2017-05-19 03:45:52 +0300

    initial commit

--- capture.py
+++ capture.py
@@ -24,15 +24,11 @@ center_img = img.copy()
 
 contours = g.find_contours_output
 cv2.imwrite('image.png', img)
-cv2.drawContours(contours_img, contours, -1, (0, 255, 0), 3)
+cv2.drawContours(contours_img, contours, -1, (0, 0, 255), 3)
 
-i = 0
-for contour in contours:
-    contour_img = img.copy()
-    cv2.drawContours(contour_img, [contour], -1, (0, 255, 0), 3)
-    cv2.imwrite('image_contour' + str(i) + '.png', contour_img)
-    i += 1
-
+for cnt in contours:
+    bottommost = tuple(cnt[cnt[:, :, 1].argmax()][0])
+    cv2.circle(contours_img, bottommost, 2, (0, 255, 0), thickness=1, lineType=8, shift=0)
 cv2.imwrite('image_contours.png', contours_img)
 center, contour = find_position(contours)
 center = (int((center[0] + 0.5) * WIDTH), int((center[1]+0.5) * HEIGHT))
